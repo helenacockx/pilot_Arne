edbe986df77fe2b7b1fe85cffcfb142ed48e48f3
helenacockx <h.cockx@donders.ru.nl>
2021-06-14 12:03:59 +0200

    detect foot stomps based on vertical acceleration of right foot

--- footstomping_intervals.m
+++ footstomping_intervals.m
@@ -1,38 +1,66 @@
-cd \\mbneufy3-srv.science.ru.nl\mbneufy3\Data_ME\source_standard\sub-06\motion
+clear all
+close all
+
+cd \\mbneufy3-srv.science.ru.nl\mbneufy3\Data_ME\source_standard\sub-21\motion
+ addpath C:\Users\helen\Documents\MATLAB\matlab_toolboxes\fieldtrip\external\xdf
 
 % load data
 cfg=[];
-cfg.datafile= 'sub-06_task-footnonauto_motion.mvnx';
+cfg.datafile= 'sub-21_task-footnonauto_motion.mvnx';
 data_motion=ft_preprocessing(cfg);
 
-% select foot contacts right foot
+% select vertical acceleration of right foot
 cfg=[];
-cfg.channel={'fc_RightFoot_Heel_footContacts', 'fc_RightFoot_Toe_footContacts'};
-data_FC=ft_selectdata(cfg, data_motion);
+cfg.channel={'sen_RightFoot_sensorFreeAcceleration_Z'};
+data_accel=ft_selectdata(cfg, data_motion);
 
-% plot
-figure; plot(data_FC.time{1}, data_FC.trial{1}(1,:))
-ylim([-1 2])
-hold on; plot(data_FC.time{1}, data_FC.trial{1}(2,:))
+ % find trials based on events
+streams=load_xdf('\\mbneufy3-srv.science.ru.nl\mbneufy3\Data_ME\source_standard\sub-21\stim\sub-21_task-automaticity_rec-01_triggerslabrecorder.xdf');
+% find lsldert04
+for i=1:length(streams)
+  if strcmp(streams{i}.info.hostname, 'lsldert04')
+    stream=streams{i}; 
+  else
+    continue
+  end
+end
+events=table(stream.time_series', stream.time_stamps', 'VariableNames', {'value', 'timestamp'});
+% find the relevant events
+idx_begin=find(strcmp(events.value, 'foot_nonauto'));
+idx_end=idx_begin+1; % 'rest'
+% find the start of the recording of this datafile
+idx_start_rec=idx_begin(1)-2; % this is usually 2 events before the first task event
+if ~strcmp(events.value(idx_start_rec), 'start_rec')
+  error('wrong start_rec event was selected')
+end
+% find relative begin and end times based on the start rec
+begin_time=events.timestamp(idx_begin)-events.timestamp(idx_start_rec);
+end_time=events.timestamp(idx_end)-events.timestamp(idx_start_rec);
+% in samples
+trials_begin=floor((begin_time-2)*data_accel.fsample); % 2 second margin
+trials_end=ceil((end_time+2)*data_accel.fsample);
 
-% find blocks
- blocks=findchangepts(data_FC.trial{1}(1,:), 'MaxNumChanges', 22)
- figure; plot(data_FC.trial{1}(1,:)); ylim([-1 2])
- hold on; plot(blocks, zeros(1, length(blocks)), 'ro')
- blocks_begin=blocks([1:2:21])-1*data_FC.fsample;% use 3-second margins
- blocks_end=blocks([2:2:22])+1*data_FC.fsample;% use 3-second margins
- 
  % split trials
  cfg=[];
- cfg.trl=[blocks_begin' blocks_end' zeros(length(blocks_begin), 1)];
- data_trl=ft_redefinetrial(cfg, data_FC);
+ cfg.trl=[trials_begin trials_end zeros(length(trials_begin), 1)];
+ data_trl=ft_redefinetrial(cfg, data_accel);
  
- % loop over trials and substract the intervals 
+ %% loop over trials and substract the intervals 
  for i=1:length(data_trl.trial)
-   figure; plot(data_trl.trial{i}(2,:)); ylim([-1 2]); title(sprintf('trial %.0d', i))
-   x=findchangepts(data_trl.trial{i}(2,:), 'MaxNumChanges', 24);
-%    hold on; plot(x, ones(1,length(x)), 'ro')
-   stomps=x(2:2:24);
-   hold on; plot(stomps, ones(1,length(stomps)), 'go')
+   figure; plot(data_trl.trial{i}(1,:)); title(sprintf('trial %.0d', i))
+   % remark: we could also first smooth the data (movmedian) and then find the local
+   % maxima
+   peaks=find(islocalmax(data_trl.trial{i}(1,:), 'MaxNumExtrema', 13, 'MinSeparation', 0.3*data_trl.fsample, 'MinProminence', 10));
+   hold on; plot(peaks, 20*ones(1,length(peaks)), 'ro')
+   legend({'vertical acceleration of the right foot', 'detected foot stomps'})
+   if length(peaks)>=12
+     stomps=peaks(1:12); % only select the first 12
+   else 
+     warning('Not all 12 foot stomps were detected for trial %.0d', i)
+     stomps=peaks;
+   end
    intervals=diff(stomps)/data_trl.fsample
  end
+
+
+
